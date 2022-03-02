348970c3724c11c5e428436033f3a103f02cf0e9
Patricia Novaes <patricia.novaes@gmail.com>
2022-03-02 20:52:07 -0300

    first commit

---
+++ src/main/java/br/com/letscode/demoproduto/DemoProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.demoproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProdutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/letscode/demoproduto/DemoProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.demoproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProdutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
