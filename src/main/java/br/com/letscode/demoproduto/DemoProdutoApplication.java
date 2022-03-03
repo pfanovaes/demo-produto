1982268c493fcc4d7c4785ee66673c90b2f08318
Patricia Novaes <patricia.novaes@gmail.com>
2022-03-02 21:18:13 -0300

    Aula 5 - Criação do projeto

--- src/main/java/br/com/letscode/demoproduto/DemoProdutoApplication.java
+++ src/main/java/br/com/letscode/demoproduto/DemoProdutoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoProdutoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoProdutoApplication.class, args);
     }
 
