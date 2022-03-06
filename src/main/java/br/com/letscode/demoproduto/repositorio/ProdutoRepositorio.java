b80319b2943ecdb21ee079cb04f65c5edd9c51fd
Patricia Novaes <patricia.novaes@gmail.com>
2022-03-05 22:00:06 -0300

    Aula 6

--- src/main/java/br/com/letscode/demoproduto/repositorio/ProdutoRepositorio.java
+++ src/main/java/br/com/letscode/demoproduto/repositorio/ProdutoRepositorio.java
@@ -4,13 +4,19 @@ import br.com.letscode.demoproduto.entidade.ProdutoEntidade;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ProdutoRepositorio {
 
     private static List<ProdutoEntidade> lista = new ArrayList<>();
 
-    public void salvar(ProdutoEntidade produtoEntidade) {
-            lista.add(produtoEntidade);
+    public ProdutoEntidade salvar(ProdutoEntidade produtoEntidade) {
+        String id = UUID.randomUUID().toString();
+        produtoEntidade.setId(id);
+
+        lista.add(produtoEntidade);
+
+        return produtoEntidade;
     }
 
     public List<ProdutoEntidade> obterTodos() {
